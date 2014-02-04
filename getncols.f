540a015d09a67843f0cbd1d4d3cb6698b2127369
Michael Murphy <mmurphy@swin.edu.au>
2014-02-04 11:02:03 +1100

    Replacing links with the files they pointed to. Sorry.

--- getncols.f
+++ getncols.f
@@ -1,1 +1,42 @@
-../getncols.f
+c     MM 27/11/00 Subroutine to find the number of columns contained in
+c     an array of character strings. The first array element is checked
+c     and then the rest of the elements are checked to make sure they
+c     contain the same number of columns
+      subroutine getncols(charline,nchar,nlines,ncols,err)
+      implicit none
+      integer j,i,nchar,nlines,ncols,ncols_new
+      character*(*) charline(nlines)
+      logical err
+
+      err=.false.
+      ncols_new=0
+
+      do j=1,nlines
+         if ((j.eq.2).or.(j.eq.1)) ncols=ncols_new
+         if (ncols.ne.ncols_new) then
+            err=.true.
+            return
+         endif
+c     Find the first non-space
+         i=0
+ 2       i=i+1
+         if (charline(j)(i:i).eq.' ') goto 2
+c     Scan whole line to see how many columns of data there are
+         ncols_new=0
+ 3       i=i+1
+         if (i.gt.nchar) goto 4
+         if (charline(j)(i:i).ne.' ') then
+            goto 3
+         elseif ((charline(j)(i:i).eq.' ').and.
+     :           (charline(j)(i-1:i-1).ne.' ')) then
+            ncols_new=ncols_new+1
+            goto 3
+         else
+            goto 3
+         endif
+ 4       if (charline(j)(nchar:nchar).ne.' ') ncols_new=ncols_new+1
+      enddo
+
+      return
+      end
+
--- polint.f
+++ polint.f
@@ -1,1 +1,50 @@
-../polint.f
+c     MM 29/8/00 Subroutine to do an n-point polynomial interpolation on
+c     an array of length n. If the calling program enters arrays longer
+c     than n then offsets can be used. Taken from Numerical Recipes and
+c     made into double precision.
+      subroutine polint(xa,ya,n,x,y,dy)
+      implicit none
+      integer n,nmax
+      parameter (nmax=10)
+      double precision dy,x,y,xa(n),ya(n)
+      double precision den,dif,dift,ho,hp,w,c(nmax),d(nmax)
+      integer i,m,ns
+
+      ns=1
+      dif=dabs(x-xa(1))
+      do i=1,n
+         dift=dabs(x-xa(i))
+         if (dift.lt.dif) then
+            ns=i
+            dif=dift
+         endif
+         c(i)=ya(i)
+         d(i)=ya(i)
+      enddo
+      y=ya(ns)
+      ns=ns-1
+      do m=1,n-1
+         do i=1,n-m
+            ho=xa(i)-x
+            hp=xa(i+m)-x
+            w=c(i+1)-d(i)
+            den=ho-hp
+            if (den.eq.0) then
+               WRITE(*,'(a)') 'STOPPING: XAs are identical in polint!'
+               stop
+            endif
+            den=w/den
+            d(i)=hp*den
+            c(i)=ho*den
+         enddo
+         if (2*ns.lt.n-m) then
+            dy=c(ns+1)
+         else
+            dy=d(ns)
+            ns=ns-1
+         endif
+         y=y+dy
+      enddo
+
+      return
+      end
